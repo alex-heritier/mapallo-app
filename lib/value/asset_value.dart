e356cebe439d8f2ec78bbe03d694c5dc3f169ae7
alex-heritier <alex.heritier@gmail.com>
2020-02-05 16:59:22 -0800

    changed android launch image

--- lib/value/asset_value.dart
+++ lib/value/asset_value.dart
@@ -4,7 +4,7 @@ abstract class AssetValue {
   static const String IMG_MAPALLO_BLUE = "$ASSET_BASE/mapallo_blue.jpg";
   static const String IMG_MAPALLO_BLACK = "$ASSET_BASE/mapallo_black.jpg";
   static const String IMG_BOAT = "$ASSET_BASE/boat.jpg";
-  static const String IMG_EARTH = "$ASSET_BASE/earth.jpg";
+  static const String IMG_EARTH = "$ASSET_BASE/earth.png";
   static const String IMG_LOGO = "$ASSET_BASE/logo.png.jpg";
   static const String IMG_BANNER = "$ASSET_BASE/mapallo_banner.png";
   static const String IMG_RIVER = "$ASSET_BASE/river.jpg";
